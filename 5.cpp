759dc41b521f275b78944065499f50a57a1e79d5
Jose <japomap@unjbg.edu.pe>
2024-06-13 23:44:46 -0500

    Agregar un ejercicio de solicitar notas de estudiantes de estructuras repetitivas

---
+++ 5.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+
+using namespace std; 
+
+int main()
+{
+	string opciones;
+	int n, suma, cont, desaprobados, aprobados;
+	float promedio;	
+	suma=0;
+	cont=0;
+	desaprobados=0;
+	aprobados=0;
+	
+	do
+	{
+		cout<<"Registre las notas de los estudiantes: ";
+		cin>>n;
+		suma+=n;		
+		cont++;
+		if(n>10.5)
+		{
+			aprobados++;
+		}
+		else 
+		{
+			desaprobados++;
+		}
+
+		cout<<"Desea continuar (si/no): ";
+		cin>>opciones;
+	}
+	while(opciones=="si");
+
+	if(cont> 0)
+	{
+		promedio=suma/cont;
+		cout<<"El promedio de las notas es "<<promedio<<endl;
+		cout<<"aprobados: "<<aprobados<<endl;
+		cout<<"desaprobados: "<<desaprobados<<endl;
+	}
+	
+	else
+	{
+		cout<<"No se ingresaron notas"<<endl;
+	}
+	
+	return 0;
+		
+}
