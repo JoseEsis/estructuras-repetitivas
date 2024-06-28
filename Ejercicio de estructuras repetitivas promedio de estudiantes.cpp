2bad47cc23790f53630415a8a8505450d6cb0c7b
TheFallenUSER <eduardopea616@gmail.com>
2024-06-28 15:10:09 -0500

    Correcciones Menores: Reintegración de la librería string y añadida validación para valores positivos de la nota de los estudiantes

--- Ejercicio de estructuras repetitivas promedio de estudiantes.cpp
+++ Ejercicio de estructuras repetitivas promedio de estudiantes.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
+#include <string>
+#include <locale.h> //Llamado a la librería que nos permitirá utilizar ñ y acentos
 using namespace std;
 
-int main()
-{	
+int main(){
+	setlocale(LC_ALL, "spanish"); //Uso y definición de la función setlocale
 	int n,suma=0,cont=0,desaprobados=0,aprobados=0;
 	string opciones,nombre,nombreEstudiante,RegistroNombre="";
-	float promedio;
 	cout<<"\nBIENVENIDO AL REGISTRO DE NOTAS"<<endl;
 	cout<<"\nRegistre su nombre: ";
 	cin>>nombre;
-	do
-	{
-		cout<<"\nRegistre nombre del estudiante: ";
+	do{
+		cout<<"\nRegistre nombre del estudiante (no ingrese ñ ni acentos): ";
 		cin>>nombreEstudiante;
-		cout<<"Registre la nota del estudiante (solo ingresar numeros): ";
-		cin>>n;
+		do{
+			cout<<"Registre la nota del estudiante (solo ingresar numeros): ";
+			cin>>n;
+			if(n<0){
+				cout<<"ERROR: Valor númerico no admitido"<<endl; //Se verifica que la nota no sea valor menor a 0
+			}
+		}while(n<0);
+		if(n>10.5){
+			aprobados++;
+			RegistroNombre+=to_string(cont)+". El estudiante "+nombreEstudiante+" aprobó.\n"; //los estudiantes son enumerados y mostrados como aprobados
+		}else{
+			desaprobados++;
+			RegistroNombre+=to_string(cont)+". El estudiante "+nombreEstudiante+" desaprobó.\n"; //Los estudiantes son enumerados y mostrados como desaprobados
+		}
 		suma+=n;		
 		cont++;
-		if(n>10.5)
-		{
-			aprobados++;
-			RegistroNombre+="- El estudiante "+nombreEstudiante+" ha aprobado\n"; //los estudiantes son enumerados y mostrados como aprobados
-		}else 
-		{
-			desaprobados++;
-			RegistroNombre+="- El estudiante "+nombreEstudiante+" ha desaprobado\n"; //Los estudiantes son enumerados y mostrados como desaprobados
-		}
-
 		cout<<"Desea continuar? (si/no): ";
 		cin>>opciones;
-	}
-	while(opciones=="si");		
-	promedio=suma/cont;
+	}while(opciones=="si"||opciones=="SI"||opciones=="Si"||opciones=="s"||opciones=="S");		
+	double promedio=double(suma)/cont; //Cambio del tipo de variable por una de mayor precisión
 	cout<<"\nDatos obtenidos"<<endl;
 	cout<<"\nNombre del registrador de notas: "<<nombre<<endl; //Se denomina al usuario como registrador de notas
 	cout<<"El promedio de las notas es: "<<promedio<<endl;
